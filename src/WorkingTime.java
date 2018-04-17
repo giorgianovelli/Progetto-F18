161a12c16edd2974f99970b045ebe6fe9848ea35
btissame-akrach <btissame.arkach@hotmail.com>
2018-04-17 16:16:11 +0200

    I have edited WorkingTime class

--- src/WorkingTime.java
+++ src/WorkingTime.java
@@ -1,5 +1,13 @@
+import java.sql.Time;
 import java.util.*;
 
 public class WorkingTime {
-    
+    private Time start;
+    private Time end;
+
+    public WorkingTime(Time start, Time end) {
+        this.start = start;
+        this.end = end;
+    }
+
 }
