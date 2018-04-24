4d9f3036cab33cae0297934a74369e790d6b3cea
nicolas-carolo <nicolas.carolo@outlook.com>
2018-04-24 09:08:28 +0200

    I have added SearchEngine and Bank classes

---
+++ src/SearchEngine.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+
+public class SearchEngine {
+    private HashSet<DogSitter> dogSitterList;
+
+    public SearchEngine() {
+        //inserire la funzione che scarica la lista di dogsitter dal database
+        //per costruire l'HashSet con tutti i dog sitter registrati alla piattaforma
+    }
+}
