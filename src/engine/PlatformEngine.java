bcca59f0b5f381bbe467d8403d92e68df88d7f24
nicolas <nicolas.carolo@outlook.com>
2018-05-10 23:31:02 +0200

    I have implemented the static method for rounding double up to second decimal

--- src/engine/PlatformEngine.java
+++ src/engine/PlatformEngine.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 import static java.lang.Math.floor;
+import static staticClasses.DoubleTools.round2Decimal;
 import static staticClasses.ObjectCreator.createDogSitterFromDB;
 import static staticClasses.dateTime.DateTimeTools.dateTimeDiff;
 
@@ -239,7 +240,8 @@ public class PlatformEngine {
             price = price + (priceMap.get(d.getSize()) * hRange.get(3) * minutesHourRatio);
         }
         //arrotondamento alla seconda cifra decimale
-        return (double)Math.round(price*100)/100;
+        //return (double)Math.round(price*100)/100;
+        return round2Decimal(price);
     }
 
 }
