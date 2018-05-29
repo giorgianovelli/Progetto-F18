11ba72f6c04d4ca2917c4db980d6157cbd065e3f
nicolas-carolo <nicolas.carolo@outlook.com>
2018-05-29 10:20:42 +0200

    Now GUIListAssignment supports client-server architecture

--- src/customerClient/gui/GUIListAssignments.java
+++ src/customerClient/gui/GUIListAssignments.java
@@ -1,5 +1,6 @@
 package customerClient.gui;
 
+import customerClient.CustomerProxy;
 import database.DBConnector;
 import server.Assignment;
 import server.Customer;
@@ -12,6 +13,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class GUIListAssignments extends JFrame{
 
@@ -28,29 +30,35 @@ public class GUIListAssignments extends JFrame{
     private JPanel contentPanel = new JPanel(); //pannello esterno
     private JScrollPane scrollPanel = new JScrollPane(contentPanel);
 
-    private JLabel labelState[]; //Da Fare
+    private JLabel labelState[]; //TODO
     private JLabel[] labelDescription; //non va a capo, trovare un alternativa
     private JButton[] buttonAction;
     private JPanel[] infoPanel;  //infopanel[i] contiene una label e un bottone
-    private Customer customer;
+    //private Customer customer;
+    private HashMap<Integer, Assignment> listAssignment;
+    private CustomerProxy proxy;
 
-    public GUIListAssignments(CalendarState cs, Customer customer, GUICustomer guiCustomer){
+    public GUIListAssignments(CalendarState cs, HashMap<Integer, Assignment> listAssignment, CustomerProxy proxy, GUICustomer guiCustomer){
         setTitle("Your assignments");
         setSize(WIDTH, HEIGHT);
         setLocation((screenSize.width - getWidth()) / 2, (screenSize.height - getHeight()) / 2);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
         setLayout(new BorderLayout());
-        this.customer = customer;
+        this.listAssignment = listAssignment;
+        this.proxy = proxy;
 
-        initComponents(cs, customer, guiCustomer);
+
+        initComponents(cs, guiCustomer);
     }
 
-    private void initComponents(CalendarState cs, Customer customer, GUICustomer guiCustomer){
-        //DA FARE: stato dell'assignment, e vedere se funziona la visualizzazione delle recensioni
+    private void initComponents(CalendarState cs, GUICustomer guiCustomer){
+        //TODO stato dell'assignment, e vedere se funziona la visualizzazione delle recensioni
 
-        assignmentNumber = customer.getAssignmentList().size();
-        reviewNumber = customer.getReviewList().size();
+        assignmentNumber = listAssignment.size();
+        //assignmentNumber = customer.getAssignmentList().size();
+        //reviewNumber = customer.getReviewList().size();
+        reviewNumber = 0;   //TODO
 
 
         if(cs.equals(CalendarState.DELETING_REVIEW)|| cs.equals(CalendarState.SHOW_REVIEWS)){ //da controllare
@@ -69,37 +77,24 @@ public class GUIListAssignments extends JFrame{
 
 
 
-        DBConnector dbConnector = new DBConnector();
+        //DBConnector dbConnector = new DBConnector();
 
 
         if (cs.equals(CalendarState.REVIEWING)){
             setTitle("Write a review");
             int j = 0;
-            for(Integer i : customer.getAssignmentList().keySet()){
+            for(Integer i : listAssignment.keySet()){
                 Assignment a = null;
-                try{
-                    //int j = i + 1;
-                    String s;
-                    a = customer.getAssignmentList().get(i);
+                //int j = i + 1;
+                String s;
+                a = listAssignment.get(i);
+                String nameDogSitter = proxy.getDogSitterNameOfAssignment(a.getCode());
+                String surnameDogSitter = proxy.getDogSitterSurnameOfAssignment(a.getCode());
 
-
-                    ResultSet rs = dbConnector.askDB("SELECT DOGSITTER FROM ASSIGNMENT WHERE CODE = '"+ a.getCode()+ "'");
-                    rs.next();
-                    String temp = rs.getString("DOGSITTER");
-                    ResultSet rs2 = dbConnector.askDB("SELECT NAME, SURNAME FROM DOGSITTERS WHERE EMAIL = '"+temp+"'");
-                    rs2.next();
-                    s = "Assignment with " + rs2.getString("NAME") + " " + rs2.getString("SURNAME") + " on " + a.getDateStart();
-
-                    labelDescription[j]= new JLabel(s);
-                    buttonAction[j]= new JButton("Write a review");
-
-                    createPanel(j);
-                    j++;
-
-                }catch(SQLException e){
-                    System.out.println(e.getMessage());
-                }
-
+                labelDescription[j]= new JLabel(nameDogSitter + " " + surnameDogSitter);
+                buttonAction[j]= new JButton("Write a review");
+                createPanel(j);
+                j++;
             }
 
 
@@ -109,7 +104,8 @@ public class GUIListAssignments extends JFrame{
         else if (cs.equals(CalendarState.DELETING_REVIEW)){ //DA controllare!!!!!
             setTitle("Your reviews");
             int j = 0;
-            for(Integer i: customer.getReviewList().keySet()){
+            //TODO
+            /*for(Integer i: customer.getReviewList().keySet()){
                 Review r = null;
                 String s;
                 r = customer.getReviewList().get(i);
@@ -120,7 +116,7 @@ public class GUIListAssignments extends JFrame{
                 createPanel(j);
                 j++;
 
-            }
+            }*/
 
 
 
@@ -130,7 +126,8 @@ public class GUIListAssignments extends JFrame{
         else if (cs.equals(CalendarState.SHOW_REVIEWS)){ //DA controllare!!!!!
             setTitle("Your reviews");
             int j = 0;
-            for(Integer i: customer.getReviewList().keySet()){
+            //TODO
+            /*for(Integer i: customer.getReviewList().keySet()){
                 Review r = null;
                 String s;
                 r = customer.getReviewList().get(i);
@@ -141,37 +138,34 @@ public class GUIListAssignments extends JFrame{
                 createPanel(j);
                 j++;
 
-            }
+            }*/
 
 
 
 
         } else {
             int j = 0;
-            for(Integer i : customer.getAssignmentList().keySet()){
+            for(Integer i : listAssignment.keySet()){
                 Assignment a = null;
-                try{
-                    //int j = i + 1;
-                    String s;
-                    a = customer.getAssignmentList().get(i);
+                //int j = i + 1;
+                String s;
+                a = listAssignment.get(i);
 
+                /* ResultSet rs = dbConnector.askDB("SELECT DOGSITTER FROM ASSIGNMENT WHERE CODE = '"+ a.getCode()+ "'");
+                   rs.next();
+                   String temp = rs.getString("DOGSITTER");
+                   ResultSet rs2 = dbConnector.askDB("SELECT NAME, SURNAME FROM DOGSITTERS WHERE EMAIL = '"+ temp +"'");
+                   rs2.next();
+                   s = "Assignment with " + rs2.getString("NAME") + " " + rs2.getString("SURNAME");*/
 
-                    ResultSet rs = dbConnector.askDB("SELECT DOGSITTER FROM ASSIGNMENT WHERE CODE = '"+ a.getCode()+ "'");
-                    rs.next();
-                    String temp = rs.getString("DOGSITTER");
-                    ResultSet rs2 = dbConnector.askDB("SELECT NAME, SURNAME FROM DOGSITTERS WHERE EMAIL = '"+ temp +"'");
-                    rs2.next();
-                    s = "Assignment with " + rs2.getString("NAME") + " " + rs2.getString("SURNAME");
+                String nameDogSitter = proxy.getDogSitterNameOfAssignment(a.getCode());
+                String surnameDogSitter = proxy.getDogSitterSurnameOfAssignment(a.getCode());
 
-                    labelDescription[j]= new JLabel(s);
-                    buttonAction[j]= new JButton("Info");
+                labelDescription[j]= new JLabel(nameDogSitter + " " + surnameDogSitter);
+                buttonAction[j]= new JButton("Info");
 
-                    createPanel(j);
-                    j++;
-
-                }catch(SQLException e){
-                    System.out.println(e.getMessage());
-                }
+                createPanel(j);
+                j++;
 
             }
 
