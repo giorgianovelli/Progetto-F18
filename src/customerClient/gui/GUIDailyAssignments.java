41aebad7f5b8e295c5320f1bd418573e994d303d
btissame-akrach <btissame.akrach@hotmail.com>
2018-06-04 14:49:34 +0200

    Bottone Action Listener -Non funzionante

--- src/customerClient/gui/GUIDailyAssignments.java
+++ src/customerClient/gui/GUIDailyAssignments.java
@@ -2,9 +2,11 @@ package customerClient.gui;
 
 import server.Customer;
 import enumeration.CalendarState;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.*;
 
 public class GUIDailyAssignments extends JFrame {
     final int WIDTH = 512;
@@ -14,6 +16,7 @@ public class GUIDailyAssignments extends JFrame {
     private JButton button[];
     private JLabel lb = new JLabel();
     private JScrollPane scroll = new JScrollPane(p);
+    private JFrame frame = new JFrame();
 
     private Customer customer;
 
@@ -26,15 +29,13 @@ public class GUIDailyAssignments extends JFrame {
         setResizable(false);
         setLayout(new BorderLayout());
         setVisible(true);
-        initComponents (cs);
+        initComponents(cs);
         this.customer = customer;
     }
 
-    private void initComponents(CalendarState cs){
-        //metodo da implementare
-
+    private void initComponents(CalendarState cs) {
         //inserire query per interrogare db
-        int nAssignments = 30;
+        int nAssignments = 6;
 
         button = new JButton[nAssignments];
 
@@ -47,24 +48,56 @@ public class GUIDailyAssignments extends JFrame {
                 button[i] = new JButton("Delete");
                 p.add(lb);
                 p.add(button[i]);
+                p.add(frame);
+               // lb.setText("Delete");
+                button[nAssignments].addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                        JDialog d = new JDialog(frame, "Conferm Actions", true);
+                        d.setLocationRelativeTo(frame);
+                        d.setVisible(true);
+                    }
+                });
+
+                frame.getContentPane().setLayout(new FlowLayout());
+                frame.add(button[nAssignments]);
+                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                frame.setPreferredSize(new Dimension(512, 512));
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
             }
-        } else if (cs.equals(CalendarState.NORMAL)){
-            setTitle("Daily assignment");
-            p.setLayout(new GridLayout(nAssignments, 2));
-            for (int i = 0; i < nAssignments; i++) {
-                int n = i + 1;
-                lb = new JLabel("Daily assignment n° " + n);
-                button[i] = new JButton("Info");
-                p.add(lb);
-                p.add(button[i]);
             }
 
-
-
+          else if(cs.equals(CalendarState.NORMAL)) {
+        setTitle("Daily assignment");
+        p.setLayout(new GridLayout(nAssignments, 2));
+        for (int i = 0; i < nAssignments; i++) {
+            int n = i + 1;
+            lb = new JLabel("Daily assignment n° " + n);
+            button[i] = new JButton("Info");
+            p.add(lb);
+            p.add(button[i]);
         }
+
+
+    }
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         getContentPane().add(scroll);
 
 
-    }
 }
+    public void main(String[] args)
+    {
+
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run()
+            {
+
+            }
+        });
+    }
+    }
+
+
