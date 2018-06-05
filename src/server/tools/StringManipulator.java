0d2f70566b0988338890c23ea1a7a44519ee5349
nicolas-carolo <nicolas.carolo@outlook.com>
2018-06-05 14:40:00 +0200

    I have fixed a bug in StringManipulator class

--- src/server/tools/StringManipulator.java
+++ src/server/tools/StringManipulator.java
@@ -1,13 +1,32 @@
 package server.tools;
 
+import java.util.StringTokenizer;
+
 public class StringManipulator {
     public static String capitalizeFirstLetter(String strToCapitalize){
         if (strToCapitalize.length() > 0){
+            if (strToCapitalize.contains(" ")){
+                return capitalizeFirstLetterForEachWord(strToCapitalize);
+            }
             strToCapitalize = strToCapitalize.toLowerCase();
             return strToCapitalize.substring(0, 1).toUpperCase() + strToCapitalize.substring(1, strToCapitalize.length());
         } else {
             return "";
         }
 
+    }
+
+    private static String capitalizeFirstLetterForEachWord(String strToCapitalize){
+        strToCapitalize = strToCapitalize.toLowerCase();
+        StringTokenizer strToken = new StringTokenizer(strToCapitalize, " ");
+        String toReturn = "";
+        while (strToken.hasMoreTokens()){
+            String word = strToken.nextToken();
+            toReturn = toReturn + word.substring(0, 1).toUpperCase() + word.substring(1, word.length());
+            if (strToken.hasMoreTokens()){
+                toReturn = toReturn + " ";
+            }
+        }
+        return toReturn;
     }
 }
