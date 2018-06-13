3d4a9f7fa95d373f3acdb9aaf46831b3971f04d9
btissame-akrach <btissame.akrach@hotmail.com>
2018-06-13 20:52:01 +0200

    I have improved GUISettings

--- src/customerClient/gui/GUISettings.java
+++ src/customerClient/gui/GUISettings.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class GUISettings extends JFrame {
-    final int WIDTH = 600;
+    final int WIDTH = 512;
     final int HEIGHT = 600;
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
@@ -27,18 +27,23 @@ public class GUISettings extends JFrame {
 
     private JLabel labelName = new JLabel("Name:", SwingConstants.LEFT);
     private JLabel labelSurname = new JLabel("Surname:", SwingConstants.LEFT);
-    private JLabel labelPhoneNumber = new JLabel("Phone number:", SwingConstants.LEFT);
     private JLabel labelDate = new JLabel("Date of birth:", SwingConstants.LEFT);
     private JLabel labelAddress = new JLabel("Address:", SwingConstants.LEFT);
+    private JLabel labelCountry = new JLabel("Country:", SwingConstants.LEFT);
+    private JLabel labelCity = new JLabel("City:", SwingConstants.LEFT);
+    private JLabel labelStreet = new JLabel("Street:", SwingConstants.LEFT);
+    private JLabel labelNumber = new JLabel();
+    private JLabel labelCap = new JLabel("Cap:", SwingConstants.LEFT);
+    private JLabel labelPhoneNumber = new JLabel("Phone number:", SwingConstants.LEFT);
     private JLabel labelPaymentMethod = new JLabel("PaymentMethod:", SwingConstants.LEFT);
 
-    private JTextField textName = new JTextField(); //ho cancellato solo contenuto parentesi
+    private JTextField textName = new JTextField();
     private JTextField textSurname = new JTextField();
     private JTextField textStreet = new JTextField();
     private JTextField textNumber = new JTextField();
-    private JTextField textCap = new JTextField();
     private JTextField textCity = new JTextField();
     private JTextField textCountry = new JTextField();
+    private JTextField textCap = new JTextField();
     private JTextField textPhoneNumber = new JTextField();
 
     private JButton buttonConfirm = new JButton("Confirm");
@@ -93,7 +98,6 @@ public class GUISettings extends JFrame {
         panelData.add(labelDate);
 
 
-
         for(int years = 1930; years<= Calendar.getCurrentYear() ; years++) {
             years_tmp.add(years+"");
         }
@@ -102,19 +106,37 @@ public class GUISettings extends JFrame {
         monthList = new JComboBox<>(month);
         yearList = new JComboBox(years_tmp.toArray());
 
+        //todo  per riempire le jcombobox con le date corrette
+        Date strDate= proxy.getCustomerDateOfBirth();
+        // System.out.println(strDate);
+        SimpleDateFormat dateFormatdd = new SimpleDateFormat("dd");
+        SimpleDateFormat dateFormatmm = new SimpleDateFormat("MM");
+        SimpleDateFormat dateFormatyyy = new SimpleDateFormat("yyyy");
+
+        String day = dateFormatdd.format(strDate);
+        String month = dateFormatmm.format(strDate);
+        String year = dateFormatyyy.format(strDate);
+
+        dayList.setSelectedItem(day);
+        monthList.setSelectedItem(month);
+        yearList.setSelectedItem(year);
+
         panelDate.setLayout(new GridLayout(1,3,5,5));
         panelDate.add(dayList);
         panelDate.add(monthList);
         panelDate.add(yearList);
         panelData.add(panelDate);
+        panelData.add(labelCountry);
+        panelData.add(textCountry);
+        panelData.add(labelCity);
+        panelData.add(textCity);
+        panelData.add(labelCap);
+        panelData.add(textCap);
 
         panelData.add(labelAddress);
         panelAddress.setLayout(new BoxLayout(panelAddress,BoxLayout.X_AXIS));
         panelAddress.add(textStreet);
         panelAddress.add(textNumber);
-        panelAddress.add(textCap);
-        panelAddress.add(textCity);
-        panelAddress.add(textCountry);
         panelData.add(panelAddress);
 
         panelData.add(labelPhoneNumber);
@@ -123,7 +145,7 @@ public class GUISettings extends JFrame {
         add(panelOut);
 
         panelButton.setLayout(new GridLayout(1, 2));
-        panelButton.setBorder(BorderFactory.createEmptyBorder(10, 10, 20, 10));
+        panelButton.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         panelButton.add(buttonCancel, BorderLayout.SOUTH);
         panelButton.add(buttonConfirm, BorderLayout.SOUTH);
 
@@ -138,7 +160,9 @@ public class GUISettings extends JFrame {
 
                 if (registrationAe.getActionCommand().equals("Confirm")) {
                     setNewValues();
-
+                    JOptionPane.showMessageDialog(new JFrame(), "the data update was successful", "", JOptionPane.INFORMATION_MESSAGE);
+                    dispose();
+                    //TODO nel caso in cui non viene fatta la modifica??
                 }
                 if (registrationAe.getActionCommand().equals("Cancel")) {
                     dispose();
@@ -187,7 +211,7 @@ public class GUISettings extends JFrame {
 
     }
 
-    //TODO Metodo da sistemare: data di nascita non viene presa dal database da correggere??
+
     private void setValues() {
 
         String strName = proxy.getCustomerName();
@@ -200,30 +224,26 @@ public class GUISettings extends JFrame {
         textSurname.setEditable(true);
         labelSurname.setLabelFor(textSurname);
 
-       // SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
-       // Date strDate = proxy.getCustomerDateOfBirth();
-
         Address customerAddress = proxy.getAddress();
         textStreet.setText(customerAddress.getStreet());
         textStreet.setEditable(true);
-        labelAddress.setLabelFor(textStreet);
+        labelStreet.setLabelFor(textStreet);
 
         textNumber.setText(customerAddress.getNumber());
         textNumber.setEditable(true);
-        labelAddress.setLabelFor(textNumber);
-
-        textCap.setText(customerAddress.getCap());
-        textCap.setEditable(true);
-        labelAddress.setLabelFor(textCap);
-
-        textCity.setText(customerAddress.getCity());
-        textCity.setEditable(true);
-        labelAddress.setLabelFor(textCity);
+        labelStreet.setLabelFor(textNumber);
 
         textCountry.setText(customerAddress.getCountry());
         textCountry.setEditable(true);
-        labelAddress.setLabelFor(textCountry);
+        labelCountry.setLabelFor(textCountry);
+
+        textCity.setText(customerAddress.getCity());
+        textCity.setEditable(true);
+        labelCity.setLabelFor(textCity);
+
+        textCap.setText(customerAddress.getCap());
+        textCap.setEditable(true);
+        labelCap.setLabelFor(textCap);
 
         String strPhoneNumber = proxy.getCustomerPhoneNumber();
         textPhoneNumber.setText(strPhoneNumber);
@@ -234,11 +254,35 @@ public class GUISettings extends JFrame {
     }
 
 
-    //TODO metodo che conterrà i nuovi valori NON FUNZIONA
+    //TODO metodo per cambiare i valori aggiornati dall'utente nel database
     private void setNewValues() {
         proxy.updateCustomerName(textName.getText());
         textName.setEditable(true);
         labelName.setLabelFor(textName);
+
+        proxy.updateCustomerSurname(textSurname.getText());
+        textSurname.setEditable(true);
+        labelSurname.setLabelFor(textSurname);
+
+        //manca come aggiornare la data nelle JCombobox
+
+        proxy.updateCustomerAddress( textCountry.getText(), textCity.getText(), textStreet.getText(),textNumber.getText(), textCap.getText());
+        textCountry.setEditable(true);
+        labelCountry.setLabelFor(textCountry);
+        textCity.setEditable(true);
+        labelCity.setLabelFor(textCity);
+        textStreet.setEditable(true);
+        labelStreet.setLabelFor(textStreet);
+        textNumber.setEditable(true);
+        labelNumber.setLabelFor(textNumber);
+        textCap.setEditable(true);
+        labelCap.setLabelFor(textCap);
+
+        proxy.updateCustomerPhoneNumber(textPhoneNumber.getText());
+        textPhoneNumber.setEditable(true);
+        labelPhoneNumber.setLabelFor(textPhoneNumber);
+
+
 
 
     }
