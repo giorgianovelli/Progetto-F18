f55c13dbd30242835de9923bd73cb815fd88c8f3
giorgianovelli <32876200+giorgianovelli@users.noreply.github.com>
2018-06-20 01:32:45 +0200

    ho cambiato il layout di GUIWriteReview

--- src/customerClient/gui/GUIWriteReview.java
+++ src/customerClient/gui/GUIWriteReview.java
@@ -1,7 +1,11 @@
 package customerClient.gui;
 
+import server.Review;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUIWriteReview extends JFrame {
     final int WIDTH = 512;
@@ -11,10 +15,13 @@ public class GUIWriteReview extends JFrame {
     private JPanel contentPanel;
     private JPanel topPanel;
     private JPanel bottomPanel;
-    //private JPanel votePanel;
+    private JPanel votePanel;
     private JPanel buttonPanel;
+    private JPanel descriptionPanel;
 
-    private JLabel labetTitle;
+    private JScrollPane descriptionFieldScroll;
+
+    private JLabel labelTitle;
     private JLabel labelVote;
     private JLabel labelDescription;
 
@@ -26,7 +33,7 @@ public class GUIWriteReview extends JFrame {
     private JComboBox<String> voteBox;
     private String[] grade = new String[]{"1", "2", "3", "4", "5"};
 
-    public GUIWriteReview(){
+    public GUIWriteReview(){ //devo farmi passare l'appuntamento per aggiungere la recensione (per il codice)?
         initComponent();
     }
 
@@ -38,74 +45,97 @@ public class GUIWriteReview extends JFrame {
         setResizable(false);
 
         contentPanel = new JPanel(); //contiene toPanel, bottomPanel
+
         topPanel = new JPanel(); //contiene il titolo
-        //votePanel = new JPanel();
-
-        bottomPanel = new JPanel(); //contiene  descrizione, votazione e bottoni
-
+        votePanel = new JPanel();
+        bottomPanel = new JPanel();
         buttonPanel = new JPanel(new GridLayout(1,2, 20, 20));
+        descriptionPanel = new JPanel(new GridLayout(1,1));
 
-        labetTitle = new JLabel("Title: ");
+
+        labelTitle = new JLabel("Title: ");
         labelVote = new JLabel("Vote: ");
         labelDescription = new JLabel("Description: ");
         titleField = new JTextArea(2,1);
-        descriptionField = new JTextArea( 50,1);
+        descriptionField = new JTextArea( 10,1);
         enterButton = new JButton("Enter");
         cancelButton = new JButton("Cancel");
+
+        descriptionFieldScroll = new JScrollPane(descriptionField);
 
         voteBox = new JComboBox<>(grade);
 
         contentPanel.setLayout(new BorderLayout());
 
-        topPanel.setLayout(new GridLayout(2,2,10,10));
-        topPanel.add(labetTitle);
-        topPanel.add(labelVote);
+        topPanel.setLayout(new GridLayout(2,1,5,5));
+        labelTitle.setFont(new Font("TimesRoman", Font.BOLD, 20));
+        topPanel.add(labelTitle);
         titleField.setFont(new Font("TimesRoman", Font.PLAIN, 16));
+        titleField.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        topPanel.setBorder(BorderFactory.createEmptyBorder(5,10,5,10));
         topPanel.add(titleField);
 
-        topPanel.add(voteBox);
-
-        bottomPanel.setLayout(new GridLayout(3,1, 5, 5));
-        descriptionField.setLineWrap(true);
-        bottomPanel.add(labelDescription);
-        descriptionField.setFont(new Font("TimesRoman", Font.PLAIN, 14));
-        bottomPanel.add(descriptionField);
-
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 20, 50));
-        buttonPanel.add(enterButton);
-        buttonPanel.add(cancelButton);
-        bottomPanel.add(buttonPanel);
-
-        contentPanel.add(topPanel, BorderLayout.NORTH);
-        contentPanel.add(bottomPanel, BorderLayout.CENTER);
-
-        add(contentPanel);
-
-
-        /*topPanel.setLayout(new GridLayout(3,1,5,5));
-        topPanel.add(labetTitle);
-        topPanel.add(titleField);
-
-        votePanel.setBorder(BorderFactory.createEmptyBorder(10, 5, 20, 60));
+        votePanel.setLayout(new GridLayout(2,1));
         votePanel.add(labelVote);
         votePanel.add(voteBox);
-        topPanel.add(votePanel);
+        votePanel.setBorder(BorderFactory.createEmptyBorder(20,10,30,20));
 
+        bottomPanel.setLayout(new GridLayout(1,2));
 
-        bottomPanel.setLayout(new GridLayout(3,1, 10, 20));
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(2,5,2,10));
         bottomPanel.add(labelDescription);
-        bottomPanel.add(descriptionField);
+        bottomPanel.add(votePanel);
 
-        buttonPanel.setLayout(new GridLayout(1,2));
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 50, 20, 50));
+        descriptionField.setFont(new Font("TimesRoman", Font.PLAIN, 14));
+        descriptionField.setLineWrap(true);
+        descriptionPanel.setBorder(BorderFactory.createEmptyBorder(5,10,5,10));
+        descriptionPanel.add(descriptionFieldScroll);
+
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(60, 90, 20, 90));
+        ActionListener post = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent registrationAe) {
+
+                if (registrationAe.getActionCommand().equals("Enter")) {
+                    //TODO controlli sul testo + aggiungere la recensione nel db
+
+                    int rating = Integer.parseInt((String) voteBox.getSelectedItem());
+                    String title = titleField.getText();
+                    String comment = descriptionField.getText();
+
+                    //System.out.println(rating + title + comment);
+
+                    /*Review reviewToAdd = new Review();
+                    int code  code dell'appuntamento??
+                    Date date  data dell'appuntamento???
+                    int rating
+                    String title
+                    String comment
+                    String reply ??????????
+                     */
+
+                }
+                if (registrationAe.getActionCommand().equals("Cancel")) {
+                    dispose();
+                }
+
+            }
+        };
+        enterButton.addActionListener(post);
+        cancelButton.addActionListener(post);
+
         buttonPanel.add(enterButton);
         buttonPanel.add(cancelButton);
-        bottomPanel.add(buttonPanel);
 
+        contentPanel.setLayout(new GridLayout(4,1));
         contentPanel.add(topPanel);
-        contentPanel.add(bottomPanel);*/
+        contentPanel.add(bottomPanel);
+        contentPanel.add(descriptionPanel);
+        contentPanel.add(buttonPanel);
+
 
         add(contentPanel);
+
 
     }
 
