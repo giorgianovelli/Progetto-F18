f224c111a26c3422a087849723f5d8de5ef3f88d
riky123456 <32876084+riky123456@users.noreply.github.com>
2018-07-04 22:36:37 +0200

    Updated GUIDogsitterInfo

--- src/client/gui/GUIDogsitterInfo.java
+++ src/client/gui/GUIDogsitterInfo.java
@@ -12,10 +12,12 @@ import java.util.Map;
 public class GUIDogsitterInfo extends JFrame {
 
     final int WIDTH = 512;
-    final int HEIGHT = 512;
+    final int HEIGHT = 412;
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
 
+
+    //TODO List: media recensioni
 
     private JPanel panelOut = new JPanel();
     private JPanel panelInfo = new JPanel();
@@ -26,6 +28,7 @@ public class GUIDogsitterInfo extends JFrame {
     private JPanel panelAverage = new JPanel();
     private JPanel panelBio = new JPanel();
     private JPanel panelReviews = new JPanel();
+    private GridLayout gridLayout = new GridLayout(1,1);
 
     private JScrollPane scrollPane = new JScrollPane(panelOut);
 
@@ -34,13 +37,13 @@ public class GUIDogsitterInfo extends JFrame {
     private JLabel labelSurname = new JLabel("Surname: ");
     private JLabel labelBirth = new JLabel("Birth date: ");
     private JLabel labelAverage = new JLabel("Average reviews: ");
-    private JLabel labelBio = new JLabel("Biography: ");
+    private JLabel labelBio = new JLabel("\nBiography: ");
 
     private JLabel labelNameToBeFilled = new JLabel();
     private JLabel labelSurnameToBeFilled = new JLabel();
     private JLabel labelBirthToBeFilled = new JLabel();
     private JLabel labelAverageToBeFilled = new JLabel();
-    private JLabel labelBioToBeFilled = new JLabel();
+    private JTextArea textBiography = new JTextArea();
 
 
     private HashMap<Integer, Review> listReview;
@@ -72,12 +75,16 @@ public class GUIDogsitterInfo extends JFrame {
         panelInfoBio.setLayout(new BorderLayout());
         panelInfoBio.setBorder(BorderFactory.createTitledBorder("Bio:"));
         panelInfo.setLayout(new GridLayout(4,1));
+        panelBio.setLayout(new BorderLayout());
         panelName.setLayout(new GridLayout(1,2));
         panelSurname.setLayout(new GridLayout(1,2));
         panelBirth.setLayout(new GridLayout(1,2));
         panelAverage.setLayout(new GridLayout(1,2));
+        panelReviews.setLayout(gridLayout);
+        panelReviews.setBorder(BorderFactory.createTitledBorder("Reviews: "));
 
         panelOut.add(panelInfoBio, BorderLayout.NORTH);
+        panelOut.add(panelReviews, BorderLayout.CENTER);
 
         panelInfoBio.add(panelInfo, BorderLayout.NORTH);
         panelInfoBio.add(panelBio, BorderLayout.CENTER);
@@ -96,38 +103,51 @@ public class GUIDogsitterInfo extends JFrame {
         panelAverage.add(labelAverage);
         panelAverage.add(labelAverageToBeFilled);
 
-        panelBio.add(labelBio);
-        panelBio.add(labelBioToBeFilled);
+        panelBio.add(labelBio, BorderLayout.NORTH);
+        panelBio.add(textBiography, BorderLayout.CENTER);
+        textBiography.setEditable(false);
+        textBiography.setLineWrap(true);
+        textBiography.setWrapStyleWord(true);
 
 
         String strDate = simpleDateFormat.format(dogSitterProxy.getDateOfBirth());
+        String biography = dogSitterProxy.getBiography();
+        if (biography.length() > 50) {
+            biography.split("\n");
+        }
 
         labelNameToBeFilled.setText(dogSitterProxy.getName());
         labelSurnameToBeFilled.setText(dogSitterProxy.getSurname());
         labelBirthToBeFilled.setText(strDate);
         labelAverageToBeFilled.setText("3");
-        //labelBioToBeFilled.setText(dogSitterProxy.getBiography);
+        textBiography.setText(biography.toUpperCase());
 
         listReview = dogSitterProxy.getReviewList();
 
         for (Map.Entry<Integer, Review> entry: listReview.entrySet()) {
-            //String nameCustomer = entry.getValue()
+
+            String customer = dogSitterProxy.getCustomerNameOfAssignment(entry.getKey()) + " " + dogSitterProxy.getCustomerSurnameOfAssignment(entry.getKey());
             String title = entry.getValue().getTitle();
             String date = simpleDateFormat.format(entry.getValue().getDate());
-            String vote = String.valueOf(entry.getValue().getRating());
-            String allText;
-            
-            JLabel labelName = new JLabel("Review by TESTTEST");
-            JLabel labelTitle = new JLabel(title);
-            JLabel labelDate = new JLabel(date);
-            JLabel labelVote = new JLabel(vote);
-
-            JLabel labelReview = new JLabel();
+            //String vote = String.valueOf(entry.getValue().getRating());
+            String vote = entry.getValue().starsRating();
+            JLabel label = new JLabel("<html>" + "Review by: " + customer + "<br>" + title + "<br>" + date + "<br>" + vote + "<br/>");
             JButton buttonShowMore = new JButton("Show More");
+
+            JPanel panelLabel = new JPanel();
+            JPanel panelButton = new JPanel();
+            panelLabel.add(label);
+            panelButton.add(buttonShowMore);
+            panelButton.setBorder(BorderFactory.createEmptyBorder(10,0,0,10));
+
+
 
             JPanel panelReview = new JPanel();
             panelReview.setLayout(new BorderLayout());
-
+            panelReview.add(panelLabel, BorderLayout.WEST);
+            panelReview.add(panelButton, BorderLayout.EAST);
+            panelReviews.add(panelReview);
+            gridLayout.setRows(gridLayout.getRows() + 1);
 
         }
 
