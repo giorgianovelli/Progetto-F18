8268d554f63fefed396dc520b78a65d7d89ac771
btissame-akrach <btissame.akrach@hotmail.com>
2018-07-12 00:10:48 +0200

    GUICustomerLabel

---
+++ src/test/TestGUICustomerLabel.java
@@ -0,0 +1,18 @@
+package test;
+
+import client.gui.GUICustomerLabel;
+import client.proxy.CustomerProxy;
+import server.Dog;
+
+import java.util.HashSet;
+
+public class TestGUICustomerLabel {
+    public static void main(String[] args) throws Exception {
+        GUICustomerLabel guiCustomerLabel = new GUICustomerLabel("RICCARDOGIURA@GMAIL.COM");
+        guiCustomerLabel.setVisible(true);
+
+
+    }
+
+
+}
