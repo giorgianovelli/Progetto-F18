817c979125b314cd3b337f9a202ced1b25aae4ca
nicolas-carolo <nicolas.carolo@outlook.com>
2018-07-20 15:25:35 +0200

    Javadoc of places package

--- src/server/places/Address.java
+++ src/server/places/Address.java
@@ -1,12 +1,41 @@
 package server.places;
 
 public class Address {
+
+    /**
+     * The country.
+     */
     private String country;
+
+    /**
+     * The city.
+     */
     private String city;
+
+    /**
+     * The street.
+     */
     private String street;
+
+    /**
+     * The civic number.
+     */
     private String number;
+
+    /**
+     * The cap of the city.
+     */
     private String cap;
 
+
+    /**
+     * Create a new Address object.
+     * @param country the country.
+     * @param city the city.
+     * @param street the street.
+     * @param number the civic number.
+     * @param cap the cap of the city.
+     */
     public Address(String country, String city, String street, String number, String cap) {
         this.country = country;
         this.city = city;
@@ -15,26 +44,56 @@ public class Address {
         this.cap = cap;
     }
 
+
+    /**
+     * Get the city.
+     * @return the city.
+     */
     public String getCity() {
         return city;
     }
 
+
+    /**
+     * Get the country.
+     * @return the country.
+     */
     public String getCountry() {
         return country;
     }
 
+
+    /**
+     * Get the street.
+     * @return the street.
+     */
     public String getStreet() {
         return street;
     }
 
+
+    /**
+     * Get the civic number.
+     * @return the civic number.
+     */
     public String getNumber() {
         return number;
     }
 
+
+    /**
+     * Get the cap of the city.
+     * @return the cap of the city.
+     */
     public String getCap() {
         return cap;
     }
 
+
+    /**
+     * Create a string with all informations about the address.
+     * @return a string with all informations about the address.
+     */
     public String toString(){
         return street + ", " + number + "; " + cap + " " + city + ", " + country;
     }
--- src/server/places/Area.java
+++ src/server/places/Area.java
@@ -4,17 +4,38 @@ import java.util.HashSet;
 
 
 public class Area {
+
+    /**
+     * The HashSet of place where a dog sitter can work.
+     */
     HashSet<String> places;
 
+
+
+    /**
+     * Creare a new Area object.
+     */
     public Area() {
         this.places = new HashSet<>();
     }
 
+
+    /**
+     * Add a new city where a dog sitter can work.
+     * @param city
+     * @return
+     */
     public HashSet<String> addPlace(String city){
         places.add(city);
         return places;
     }
 
+
+    /**
+     * Remove a city where the dog sitter will not work longer.
+     * @param city
+     * @return
+     */
     public HashSet<String>removePlaces(String city){
         if (places.contains(city)){
             places.remove(city);
@@ -24,6 +45,12 @@ public class Area {
         }
     }
 
+
+    /**
+     * Check if Area object already contains the specified city.
+     * @param place the city to check.
+     * @return true if the city is already contained in Area.
+     */
     public boolean contains(String place){
         if (places.contains(place)){
             return true;
@@ -32,12 +59,21 @@ public class Area {
         }
     }
 
+
+    /**
+     * Print the list of place where a dog sitter can work.
+     */
     public void printPlaces(){
         for (String p : places) {
             System.out.println(p);
         }
     }
 
+
+    /**
+     * Get the HashSet of cities where a dog sitters can work.
+     * @return the HashSet of cities where a dog sitters can work.
+     */
     public HashSet<String> getPlaces() {
         return places;
     }
