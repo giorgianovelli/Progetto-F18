a961b49eba6214f66d12a6060a9d86e69fc08717
btissame-akrach <btissame.akrach@hotmail.com>
2018-07-21 21:02:52 +0200

    javadoc GUIDogSitterChangePassword and GUIChangePassword

--- src/client/gui/GUIDogSitterChangePassword.java
+++ src/client/gui/GUIDogSitterChangePassword.java
@@ -7,12 +7,15 @@ import javax.swing.*;
 
 public class GUIDogSitterChangePassword extends GUIChangePassword {
 
-
-    private DogSitterProxy dogSitterProxy;
     /**
-     * Constructor
-     *
+     * The dogSitter proxy.
+     */
+    private DogSitterProxy dogSitterProxy;
+
+    /**
+     *  Constructor
      * @param email : reference to the user
+     * @param guiHome
      */
     public GUIDogSitterChangePassword(String email, GUIHome guiHome) {
         super(email, guiHome);
@@ -20,6 +23,40 @@ public class GUIDogSitterChangePassword extends GUIChangePassword {
 
     }
 
+
+    /**
+     *  check if the password entered by the user matches the one entered during registration
+     * @param currentPwd password entered by the user to be checked
+     * @return true if the password entered by the user is correct, false if it is incorrect
+     */
+    protected boolean checkPassword(String currentPwd) {
+        boolean matchPassword;
+
+        String currentPwdProxy = dogSitterProxy.getPassword();
+
+        if (currentPwd.equals(currentPwdProxy)) {
+            JOptionPane.showMessageDialog(new JFrame(), "the current password is correct", "", JOptionPane.INFORMATION_MESSAGE);
+            matchPassword = true;
+
+        } else {
+            JOptionPane.showMessageDialog(new JFrame(), "Current Password is not correct!", "Password error", JOptionPane.ERROR_MESSAGE);
+            textCurrentPassword.setText("");
+            matchPassword = false;
+        }
+
+
+        return matchPassword;
+
+    }
+
+
+
+    /**
+     * check if the password entered in the "newPassword" field matches the "confirmPassword" field
+     * @param newPassword new password entered by the user
+     * @param confirmPassword  check the password entered
+     * @return true if the two passwords coincide false otherwise
+     */
    protected boolean changePasswordFields(String newPassword, String confirmPassword) {
         boolean updatePassword;
 
@@ -38,26 +75,6 @@ public class GUIDogSitterChangePassword extends GUIChangePassword {
 
         return updatePassword;
 
-
-    }
-
-    protected boolean checkPassword(String currentPwd) {
-        boolean matchPassword;
-
-        String currentPwdProxy = dogSitterProxy.getPassword();
-
-        if (currentPwd.equals(currentPwdProxy)) {
-            JOptionPane.showMessageDialog(new JFrame(), "the current password is correct", "", JOptionPane.INFORMATION_MESSAGE);
-            matchPassword = true;
-
-        } else {
-            JOptionPane.showMessageDialog(new JFrame(), "Current Password is not correct!", "Password error", JOptionPane.ERROR_MESSAGE);
-            textCurrentPassword.setText("");
-            matchPassword = false;
-        }
-
-
-        return matchPassword;
 
     }
 
